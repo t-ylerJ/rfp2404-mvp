8b07b8b008438e8d15d011ff77962cc8bcb08b40
Tyler Johnson <tylerj0937@gmail.com>
2024-07-09 12:39:25 -0700

    Add get methods and include env variables

--- flightSF/server/index.js
+++ flightSF/server/index.js
@@ -1,16 +1,39 @@
-const path = require('path');
 const express = require('express');
-const bodyParser = require('body-parser'); //fix
-const morgan = require('morgan'); //
+const axios = require('axios')
+const morgan = require('morgan');
+require('dotenv').config();
 
 const app = express();
+const PORT = process.env.port || 3000;
+const API_SERVER_URL = '';
 
 
-
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, '../assets')))//
+app.use(express.static('/Users/tylerjohnson/hackreactor/rfp2404/mvp/rfp2404-mvp/flightSF/public'))
 
-app.get('/flights', (req, res)
 
-)
+app.get('/*', (req, res) => {
+  const config = {
+    method: GET, //req.method
+    url: `${API_SERVER_URL}${req.url}`,
+    headers: {
+      Authorization: process.env.API_TOKEN,
+    },
+  };
+
+  axios(config)
+    .then((response) => {
+    res.status(response.status).json(response.data)
+    })
+    .catch((error) => {
+    error.response ? res.status(error.response.status).json(error.response.data) : res.status(500).json(error.message);
+    });
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`)
+});
+
