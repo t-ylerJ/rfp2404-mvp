9b233140fd79e4af6f09572abde5285e07a0fec3
Tyler Johnson <tylerj0937@gmail.com>
2024-07-08 21:46:43 -0700

    Create frontend file structure using vite

---
+++ flightSF/webpack.config.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, '/src/index.jsx'),
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: 'babel-loader',
+        },
+        resolve: {
+          extensions: ['.js', '.jsx'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [process.env.NODE_ENV === 'production' ? MiniCSSExtractPlugin.loader : 'style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.svg$/i,
+        type: 'asset',
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCSSExtractPlugin(),
+  ],
+};
