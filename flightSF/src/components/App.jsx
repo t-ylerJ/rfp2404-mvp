7d3e3e7ba53502e6e6f9a6cf8ba3ea91e7707d5b
Tyler Johnson <tylerj0937@gmail.com>
2024-07-09 23:12:50 -0700

    Create logic to randomly select cities and ensure that correct city is included in options

--- flightSF/src/components/App.jsx
+++ flightSF/src/components/App.jsx
@@ -1,26 +1,95 @@
-import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import reactLogo from '/react.svg';
+import viteLogo from '/vite.svg';
+import plane from '/airplane.svg';
+import '../App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [selectedCity, setSelectedCity] = useState('');
+  const [showResult, setShowResult] = useState(false);
+  const [cityChoices, setCityChoices] = useState([]);
+  const [gameState, setGameState] = useState(false);
+  const [winningCity, setWinningCity] = useState('Austin');
+  const cities = [
+      'Austin',
+      'Dallas',
+      'New York',
+      'LA',
+      'Houston',
+      'Chicago',
+      'Denver',
+      'San Antonio',
+      'Boston',
+      'Helena',
+      'Orlando',
+      'Seattle'
+  ];
+cities.push(winningCity);
+
+  useEffect(() => {
+    setCityChoices(
+      cities
+        .map(value => ({ value,
+          sort: value === winningCity ?
+            Math.floor(Math.random() * 4)
+          : Math.floor(Math.random() * cities.length)}))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ value }) => value)
+        .slice(0, 4)
+    );
+  }, [])
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  setShowResult(!showResult);
+  selectedCity === winningCity ? setGameState(true) : setGameState(false);
+}
+
+const handleChange = (e) => {
+  // setSelectedCity(e.target.value);
+}
 
   return (
     <>
+      <h1>Cheapest Flight to
+        <h2>San Francisco
+        <a href="https://shorturl.at/iPqoJ" target="_blank">
+          <img src={plane} className="Plane icon" alt="Plane icon" />
+        </a>
+        </h2>
+      </h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {!showResult ? (
+
+          <form onSubmit={handleSubmit}>
+            <p>Select city with cheapest flight to SF:</p>
+            {cityChoices.map((city, index) => (
+              <span key={index}>
+                <input type="radio" id={`city${index}`} name="citySelection" value={city} onChange={handleChange} required/>
+                <label htmlFor={`city${index}`}>{city}</label>
+              </span>
+            ))}
+            <div>
+              <button type="submit">Select</button>
+            </div>
+          </form>
+        ) : (
+          <div>
+            <a href="https://react.dev" target="_blank">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+            <div>
+              {!gameState ?
+                'Congrats!' : 'Oh no!'
+              }
+
+            </div>
+            <button onClick={handleSubmit}>Try Again</button>
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
